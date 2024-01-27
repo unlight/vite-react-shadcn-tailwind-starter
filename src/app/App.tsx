8da7c86b280420ac393d6d3c691fc843ead026cf
Roman <s91630277@gmail.com>
2024-01-27 22:45:47 +0400

    feat: Added toaster demo

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import { Toaster } from '@/components/ui/sonner';
 
 import './App.css';
 
 import { HoverCardDemo } from './HoverCardDemo';
+import { SonnerDemo } from './SonnerDemo';
 
 export function App() {
   return (
@@ -10,7 +12,9 @@ export function App() {
       <main className="flex min-h-screen items-center justify-center">
         <h4>Hello from app</h4>
         <HoverCardDemo />
+        <SonnerDemo />
       </main>
+      <Toaster />
     </React.StrictMode>
   );
 }
